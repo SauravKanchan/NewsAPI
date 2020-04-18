099254459c43af16c066b1c7613d8659baa959de
SauravKanchan <sauravnk30@gmail.com>
2020-04-19 00:04:37 +0530

    Add git pull

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ async function gitPush() {
     } catch (e) {
       await exec(`git remote set-url origin ${process.env.GIT_URL}`);
     }
-    const c = await exec('git add -A && git commit -m "Update News" && git push origin master');
+    const c = await exec('git pull origin master && git add -A && git commit -m "Update News" && git push origin master');
     console.log(c)
   } catch (err) {
     console.error(err)
